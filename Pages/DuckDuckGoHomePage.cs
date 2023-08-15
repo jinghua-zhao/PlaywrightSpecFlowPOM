9b707ee8aea41118545e058a020481f4efe3c72f
Miguel Zepeda <MZepeda@opentechalliance.com>
2023-08-15 09:18:29 -0700

    Add project files.

---
+++ Pages/DuckDuckGoHomePage.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.Playwright;
+
+namespace PlaywrightSpecFlowPOM.Pages
+{
+    public class DuckDuckGoHomePage
+    {
+        // Constructors
+        private readonly IPage _user;
+
+        public DuckDuckGoHomePage(Hooks.Hooks hooks)
+        {
+            _user = hooks.User;
+        }
+
+        // Selectors
+        private ILocator SearchInput => _user.Locator("input[id='search_form_input_homepage']");
+        private ILocator SearchButton => _user.Locator("input[id='search_button_homepage']");
+
+        //Actions and Assertions
+        public async Task AssertPageContent()
+        {
+            //Assert that the correct URL has been reached
+            _user.Url.Should().Be("https://duckduckgo.com/?");
+
+            //Assert that the search input is visible
+            var searchInputVisibility = await SearchInput.IsVisibleAsync();
+            searchInputVisibility.Should().BeTrue();
+
+            // //Assert that the search button is visible
+            var searchBtnVisibility = await SearchButton.IsVisibleAsync();
+            searchBtnVisibility.Should().BeTrue();
+        }
+
+        public async Task SearchAndEnter(string searchTerm)
+        {
+            //Type the search term into the search input
+            await SearchInput.TypeAsync(searchTerm);
+
+            //Assert that the search input has the text entered
+            var searchInputInnerText = await SearchInput.InputValueAsync();
+            searchInputInnerText.Should().Be(searchTerm);
+
+            //Click the search button to submit the search
+            await SearchButton.ClickAsync();
+        }
+    }
+}
---
+++ Steps/SearchResultSteps.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using PlaywrightSpecFlowPOM.Pages;
+
+namespace PlaywrightSpecFlowPOM.Steps
+{
+    [Binding]
+    public class SearchResultSteps
+    {
+        private readonly SearchResultsPage _searchResultsPage;
+
+        public SearchResultSteps(SearchResultsPage searchResultsPage)
+        {
+            _searchResultsPage = searchResultsPage;
+        }
+
+        [Then(@"the search results show '(.*)' as the first result with link '(.*)'")]
+        public async Task ThenTheSearchResultsShowAsTheFirstResult(string expectedResult, string expectedLink)
+        {
+            //Assert the page content
+            await _searchResultsPage.AssertPageContent(expectedResult);
+
+            //Assert the first search result (hence the index of 0)
+            await _searchResultsPage.AssertSearchResultAtIndex(expectedResult, 0, expectedLink);
+        }
+    }
+}
