e481ff3900e6c75e08e503b5408f2341d3b1183c
jinghua-zhao <166082567+jinghua-zhao@users.noreply.github.com>
2024-06-18 10:47:15 -0700

    Update Hooks.cs

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -15,7 +15,7 @@ namespace PlaywrightSpecFlowPOM.Hooks
             //Initialise a browser - 'Chromium' can be changed to 'Firefox' or 'Webkit'
             var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                Headless = false // -> Use this option to be able to see your test running
+                Headless = true // -> Use this option to be able to see your test running
             });
             //Setup a browser context
             var context1 = await browser.NewContextAsync();
